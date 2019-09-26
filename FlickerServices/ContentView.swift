5117cb1ce4162b7fd51d1a4c34e687130fd1138e
Shivam Dhingra <shivam.dhingra@round.glass>
2019-09-27 00:55:48 +0530

    Initial Commit

---
+++ FlickerServices/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlickerServices
+//
+//  Created by Shivam Dhingra on 9/27/19.
+//  Copyright © 2019 Shivam Dhingra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

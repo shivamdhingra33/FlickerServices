1e6c14c0c406a9ac1b185834901dfff68d54e7dd
Shivam Dhingra <shivam.dhingra@round.glass>
2019-10-16 10:34:08 +0530

    alamofire fix

--- Package.swift
+++ Package.swift
@@ -24,12 +24,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FlickerServices",
-            dependencies: ["PhotoModel", "Pods"],
+            dependencies: ["PhotoModel","Alamofire"],
             path: "FlickerServices"
         ),
-        .target(
-            name: "Pods",
-            path: "Pods"
+        .target(name: "Alamofire",
+                path: "Pods/Alamofire"
         )
     ]
 )
